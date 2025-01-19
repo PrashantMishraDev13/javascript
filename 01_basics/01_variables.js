8b046e56ab488220207b1fc2bab311236536a840
PrashantMishraDev13 <prashantmishradev13@gmail.com>
2025-01-19 12:35:09 +0000

    Variables and Datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553;
+let accountEmail = "prashantmishradev13@gmail.com";
+var accountPassword = "123456";
+accountCity = "Lucknow";
+let accountState;
+
+// accountId = 15;
+
+accountEmail = "prashantmishra1095@gmail.com";
+accountPassword = "Stw@1310";
+accountCity = "Mumbai";
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+//console.log(accountId);
+console.table([accountId, accountEmail, accountPassword , accountCity , accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // Treat all JS code as newer version of ES
+
+// alert(3 + 3)  // we are using nodejs, not browser
+
+let name = "Prashant Mishra";
+let age = 29;
+let loggedIn = false;
+
+
+/* Primitive dataTypes */
+
+// string => ""
+// number => 2 to power 53
+// bigint
+// boolean => true/false
+// null => Standalone value
+// undefined
+// symbol => Unique
+
+
+/* Non Primitive dataTypes */
+
+// array
+// object
+
+console.log(typeof null);  // object
+console.log(typeof undeined); // undefined
+
+
+
